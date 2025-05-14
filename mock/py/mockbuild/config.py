8005c65c6f23e39fec0e06056fe801be413a28da
Scott R. Shinn <scott@atomicorp.com>
2025-05-14 10:31:24 -0400

    Adding SBOM module
    Signed-off-by: Scott R. Shinn <scott@atomicorp.com>

--- mock/py/mockbuild/config.py
+++ mock/py/mockbuild/config.py
@@ -33,7 +33,7 @@ PLUGIN_LIST = ['tmpfs', 'root_cache', 'yum_cache', 'mount', 'bind_mount',
                'lvm_root', 'compress_logs', 'sign', 'pm_request',
                'hw_info', 'procenv', 'showrc', 'rpkg_preprocessor',
                'rpmautospec', 'buildroot_lock', 'export_buildroot_image',
-               'unbreq', 'expand_spec', 'system_monitor']
+               'unbreq', 'expand_spec', 'system_monitor', 'sbom_generator']
 
 def nspawn_supported():
     """Detect some situations where the systemd-nspawn chroot code won't work"""
@@ -267,7 +267,11 @@ def setup_default_config_opts():
         'system_monitor_enable': False,
         'system_monitor_opts': {
             'interval' : 2
-        }
+        },
+        'sbom_generator_enable': False,
+        'sbom_generator_opts': {
+            'generate_sbom': True,
+        },
     }
 
     config_opts['environment'] = {
